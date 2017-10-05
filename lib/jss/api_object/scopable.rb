dd42ce4525ed371497408b7c6e33d76790e5f161
sergiobaptista <sergio.baptista@akqa.com>
2017-10-05 13:26:11 -0400

    Update scopable.rb
    setting of scope references an undefined var "new_criteria". Now references "new_scope"

--- lib/jss/api_object/scopable.rb
+++ lib/jss/api_object/scopable.rb
@@ -96,7 +96,7 @@ module JSS
     ### @return [void]
     ###
     def scope= (new_scope)
-      raise JSS::InvalidDataError, "JSS::Scopable::Scope instance required" unless new_criteria.kind_of?(JSS::Scopable::Scope)
+      raise JSS::InvalidDataError, "JSS::Scopable::Scope instance required" unless new_scope.kind_of?(JSS::Scopable::Scope)
       raise JSS::InvalidDataError, "Scope object must have target_key of :#{self.class::SCOPE_TARGET_KEY}" unless self.class::SCOPE_TARGET_KEY == new_scope.target_key
       @scope = new_scope
       @need_to_update = true
